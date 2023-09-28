cf413049712d7673c23c117d704bf5d431a73f58
girejhf3ifeifnreugn <139685777+girejhf3ifeifnreugn@users.noreply.github.com>
2023-09-28 14:04:37 +0800

    hyprland conf

---
+++ hypr/autostart.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+
+source ~/.config/hypr/lib.sh
+
+
+
+run_hook pre &
+
+swww init && swww img ~/Downloads/ghibli-japanese-walled-garden.png
+
+~/.config/hypr/waybar/start
+~/.config/hypr/swaync/start
+~/.config/hypr/eww/start
+
+nm-applet &
+blueman-applet &
+
+lxsession &
+
+brightnessctl --restore
+
+eval "sleep 2; hyprctl reload" &
+
+eval "sleep 0.5; killall flameshot; pkill flameshot" &
+eval "sleep 1; killall flameshot; pkill flameshot" &
+eval "sleep 2; killall flameshot; pkill flameshot" &
+
+run_hook post &
